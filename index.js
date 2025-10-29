0b5e9b1e4a7c5ed55d66b31c4aa7804663a85f0b
sumana-bhargava <154046473+sumana-bhargava@users.noreply.github.com>
2025-10-29 17:03:38 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length
+
+for(var i=0;i<numberOfDrumButtons;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+    var buttonPressed = this.innerHTML
+    makeSound(buttonPressed)
+    createAnimation(buttonPressed)
+})
+}
+
+function makeSound(key){
+  switch (key) {
+  case 'w':
+    var tom1 = new Audio("Sounds/tom-1.mp3")
+    tom1.play()
+    break;
+  case 'a':
+    var tom2 = new Audio("Sounds/tom-2.mp3")
+    tom2.play()
+    break;
+  case 's':
+    var tom3 = new Audio("Sounds/tom-3.mp3")
+    tom3.play()
+    break;
+  case 'd':
+    var tom4 = new Audio("Sounds/tom-4.mp3")
+    tom4.play()
+    break;
+  case 'j':
+    var snare = new Audio("Sounds/snare.mp3")
+    snare.play()
+    break;
+  case 'k':
+    var crash = new Audio("Sounds/crash.mp3")
+    crash.play()
+    break;
+  case 'l':
+    var kick = new Audio("Sounds/kick-bass.mp3")
+    kick.play()
+    break;
+    
+  default:
+    console.log(this.innerHTML)
+  }
+}
+
+document.addEventListener("keydown",function(event){
+  makeSound(event.key)
+  createAnimation(event.key)
+})
+
+function createAnimation(key){
+  var key_click = document.querySelector("."+key) // this returns .w,.k,.l,.j,....
+  key_click.classList.add("pressed")
+  
+  setTimeout(function(){
+    key_click.classList.remove("pressed")
+  },200)
+}
+
